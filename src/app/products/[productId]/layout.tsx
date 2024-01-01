c1842085453ece8b63c823442dee0b6d979674d6
rebertwalogjr <rebertwalogjr@gmail.com>
2024-01-01 20:45:59 +0800

    initial commit

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+
+  export default function ProductDetailsLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <>
+        <h1>Details :</h1>
+        {children}
+      </>
+    )
+  }
+  
---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Review not found.</h2>
+            <p>Way bigger than the maximum value.</p>
+        </div>
+    )
+}
